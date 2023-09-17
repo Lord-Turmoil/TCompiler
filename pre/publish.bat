9322818a51bc8437d5c1f910c18e7e347fd0f287
Lord Turmoil <tony-turmoil@qq.com>
2023-09-17 10:32:04 +0800

    feat: auto compress to .zip

--- pre/publish.bat
+++ pre/publish.bat
@@ -2,13 +2,25 @@
 
 @ECHO OFF
 
+SET curDir=%cd%
 SET srcDir=.\tests
 SET dstDir=.\publish
 
-::Not sure if this is needed
-::It guarantees you have a canonical path (standard form)
+:: Not sure if this is needed
+:: It guarantees you have a canonical path (standard form)
+FOR %%F IN (%srcDir%) DO SET srcDir="%%~fF"
 FOR %%F IN (%dstDir%) DO SET dstDir="%%~fF"
+
 IF EXIST "%dstDir%" RMDIR /S /Q "%dstDir%"
 MKDIR "%dstDir%"
 
 FOR /r %srcDir% %%F IN (.) DO IF "%%~fF" NEQ %dstDir% ROBOCOPY "%%F" %dstDir% *.txt
+
+:: Compress to zip file
+SETLOCAL EnableDelayedExpansion
+CD %dstDir%
+SET fileList=
+FOR /r %%F IN (*.txt) DO SET fileList=!fileList! %%~nxF
+jar -cfM 21371300.zip %fileList%
+
+CD %curDir%
