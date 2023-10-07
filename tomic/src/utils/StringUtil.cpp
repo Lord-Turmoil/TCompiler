c581a11795f3329ba07166563e36113bf4529948
Lord Turmoil <tony-turmoil@outlook.com>
2023-10-07 10:32:33 +0800

    update: check nullptr in string util

--- tomic/src/utils/StringUtil.cpp
+++ tomic/src/utils/StringUtil.cpp
@@ -15,11 +15,28 @@ namespace StringUtil
 
 bool Contains(const char* str, int ch)
 {
+    if (str == nullptr)
+    {
+        return false;
+    }
+
     return (ch != EOF) && (strchr(str, ch) != nullptr);
 }
 
 bool Equals(const char* str1, const char* str2)
 {
+    // If both are nullptr, they are equal.
+    if (str1 == str2)
+    {
+        return true;
+    }
+
+    // If one of them is nullptr, they are not equal.
+    if (str1 == nullptr || str2 == nullptr)
+    {
+        return false;
+    }
+
     return strcmp(str1, str2) == 0;
 }
 
