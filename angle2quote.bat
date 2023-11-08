0ac7e0a131c5591c71d5a82bfada30705792c785
Lord Turmoil <tony-turmoil@outlook.com>
2023-11-08 16:50:40 +0800

    update: include converter

--- angle2quote.bat
+++ angle2quote.bat
@@ -1,5 +1,5 @@
-call converter\IncludeConverter.exe -a tomic\3rd-party\mioc -i include -s include src
-call converter\IncludeConverter.exe -a tomic\3rd-party\twio -i include -s include src
-call converter\IncludeConverter.exe -a tomic -i include -s include src
-call converter\IncludeConverter.exe -a tomic -i 3rd-party\mioc\include 3rd-party\twio\include -s include src
-call converter\IncludeConverter.exe -a -i tomic\include -s src
+call converter\IncludeConverter.exe --to-quote tomic\3rd-party\mioc -i include -s include src
+call converter\IncludeConverter.exe --to-quote tomic\3rd-party\twio -i include -s include src
+call converter\IncludeConverter.exe --to-quote tomic -i include -s include src
+call converter\IncludeConverter.exe --to-quote tomic -i 3rd-party\mioc\include 3rd-party\twio\include -s include src
+call converter\IncludeConverter.exe --to-quote -i tomic\include -s src
